65fd75885f6a1ee11a81cf323d5785f14daca32d
kobayashi_shoto <kobayashi.shoto@colon.ne.jp>
2024-07-29 18:55:56 +0900

    wip

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.0"),
     .package(url: "https://github.com/pointfreeco/swift-perception", from: "1.3.4"),
     .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "1.5.3"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
+    .package(url: "https://github.com/sk409/xctest-dynamic-overlay", branch: "test-swift-9"),
     .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
   ],
   targets: [
